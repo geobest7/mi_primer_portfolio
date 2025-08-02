8a503d16c579fcda860fc8a171aaf36650d96559
Alessandro Febbrai <febbrai.alessandro@libero.it>
2025-08-02 15:52:49 +0200

    he añadido mas ruta, y la lista en home

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 # Ruta de portada con selector de idioma
 @app.route('/')
 def welcome():
-    return render_template('index.html')
-
+    lang = request.args.get('lang', 'en')
+    return render_template('index.html', lang=lang)
 
 # Ruta principal del portfolio (después de elegir idioma)
 @app.route('/home')
@@ -14,22 +14,35 @@ def home():
     lang = request.args.get('lang', 'en')
     return render_template('home.html', lang=lang)
 
-
 # Ruta /about
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    lang = request.args.get('lang', 'en')
+    return render_template('about.html', lang=lang)
 
+# Ruta /studies
+@app.route('/studies')
+def studies():
+    lang = request.args.get('lang', 'en')
+    return render_template('studies.html', lang=lang)
+
+# Ruta /experience
+@app.route('/experience')
+def experience():
+    lang = request.args.get('lang', 'en')
+    return render_template('experience.html', lang=lang)
+
+# Ruta /certifications
+@app.route('/certifications')
+def certifications():
+    lang = request.args.get('lang', 'en')
+    return render_template('certifications.html', lang=lang)
 
 # Ruta /projects
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
-
+    lang = request.args.get('lang', 'en')
+    return render_template('projects.html', lang=lang)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
